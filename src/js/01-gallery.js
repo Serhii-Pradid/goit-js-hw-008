2dbd135ee5879908ce7cf4f3273370736bb220fa
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-03-10 09:58:59 +0200

    ДЗ 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const markUp = createGalleryCard(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend' , markUp)
+
+function createGalleryCard(items) {
+return items.map(({preview, original, description}) => {
+
+    return `
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"
+    />
+  </a>`
+}).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+
