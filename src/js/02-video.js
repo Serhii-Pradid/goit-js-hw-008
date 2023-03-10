56dc7788c944f9cc8c6ac28f5e930353327e1419
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-03-10 13:47:56 +0200

    ДЗ 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+
+    player.on('timeupdate', function(currentTime) {
+        localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));    
+    });
+
+    
