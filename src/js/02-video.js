6b6954a85f52039013b671136af6bde617542482
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-03-11 09:41:09 +0200

    ДЗ 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,27 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const onPlay = function(time) {
+    localStorage.setItem("videoplayer-current-time", time.seconds)  
+}
 
+    player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.on('timeupdate', function(currentTime) {
-        localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));    
-    });
+    const currentTime = Number(localStorage.getItem("videoplayer-current-time"));
 
-    
+          player.setCurrentTime(currentTime).then(function(seconds) {
+            
+        }).catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                    
+                    break;
+        
+                default:
+                   
+                    break;
+            }
+        });
